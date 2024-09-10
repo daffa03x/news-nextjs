868e071e682cb075dbc1f6076bb5b3a15cb0826d
daffa03x <mobadaff@gmail.com>
2024-09-10 15:47:10 +0700

    layout berita & install daisy UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function NewsPage() {
           </Link>
           <input type="text" className="rounded-md bg-slate-100 w-1/4 h-9" placeholder="   Cari tokoh, topik atau peristiwa" />
         </div>
-        <hr className="mt-8 border-zinc-900" />
+        <div className="divider"></div>
         <div className="container mx-auto flex flex-wrap space-x-4 justify-center align-center mt-8 pb-8">
           {news.map((category) => (
             <div key={category.name} className="relative">
@@ -60,14 +60,39 @@ export default function NewsPage() {
         </div>
       </header>
       <section className="bg-zinc-950">
-        <main className="container mx-auto p-4">
-          <p className="text-white">
-            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem
-            Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-          </p>
+        <main className="container mx-auto py-4 px-8">
+          <div className="px-8">
+            <div className="hero min-h-80 rounded-md">
+              <div className="hero-overlay">IKLAN</div>
+            </div>
+            <div
+              className="hero min-h-64 rounded-md mt-10"
+              style={{
+                backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+              }}>
+              <div className="hero-overlay"></div>
+            </div>
+            <div className="grid grid-cols-2 gap-10 mt-10">
+              <div>
+                <div
+                  className="hero min-h-80 rounded-md"
+                  style={{
+                    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                  }}>
+                  <div className="hero-overlay"></div>
+                </div>
+              </div>
+              <div>
+                <div
+                  className="hero min-h-80 rounded-md"
+                  style={{
+                    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                  }}>
+                  <div className="hero-overlay"></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </section>
     </>
